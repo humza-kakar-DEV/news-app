ff17eba515218776a3abf1a5c2eea6b72fb91c80
Muhammad Hamza Kakar <git200331@gmail.com>
2026-06-16 17:44:50 +0500

    chore: project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "News App"
+include(":app")
+include(":core:designsystem")
+include(":core:navigation")
+include(":core:database")
+include(":core:network")
+include(":core:datastore")
+include(":core:common")
+include(":feature:home")
+include(":feature:detail")
+include(":feature:onboarding")
+include(":core:di")
