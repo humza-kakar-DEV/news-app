0b09381a5f946c0a7d27f86b01c922d6c954fd4f
Muhammad Hamza Kakar <git200331@gmail.com>
2026-07-08 15:38:33 +0500

    feat: add light and dark colors

---
+++ core/designsystem/src/main/java/com/example/designsystem/color/DarkColorScheme.kt
@@ -0,0 +1,20 @@
+package com.example.designsystem.color
+
+import androidx.compose.material3.darkColorScheme
+
+val DarkColors = darkColorScheme(
+
+    background = White,
+
+    onBackground = Black,
+
+    surface = Aria,
+
+    onSurface = Black,
+
+    primary = FantasyConsoleSky,
+
+    onPrimary = White,
+
+    error = RoyalMailRed,
+)
---
+++ core/designsystem/src/main/java/com/example/designsystem/color/LightColorScheme.kt
@@ -0,0 +1,21 @@
+package com.example.designsystem.color
+
+import androidx.compose.material3.lightColorScheme
+
+val LightColors = lightColorScheme(
+
+    background = White,
+
+    onBackground = Black,
+
+    surface = Aria,
+
+    onSurface = Black,
+
+    primary = FantasyConsoleSky,
+
+    onPrimary = White,
+
+    error = RoyalMailRed,
+
+)
