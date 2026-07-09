18bed4960e0e8a86397757b13702097b2d1fc8af
Muhammad Hamza Kakar <git200331@gmail.com>
2026-07-09 16:36:25 +0500

    feat: add shimmer effect preview

--- core/designsystem/src/main/java/com/example/designsystem/component/loading/CircularLoader.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/loading/CircularLoader.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.preview.AppPreview
--- core/designsystem/src/main/java/com/example/designsystem/component/loading/LinearLoader.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/loading/LinearLoader.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.designsystem.preview.AppPreview
+import com.example.designsystem.preview.PreviewTheme
 
 @Composable
 fun LinearLoader(
@@ -12,4 +14,12 @@ fun LinearLoader(
     LinearProgressIndicator(
         modifier = modifier.fillMaxWidth()
     )
+}
+
+@AppPreview
+@Composable
+fun LinearLoaderPreview() {
+    PreviewTheme {
+        LinearLoader(modifier = Modifier)
+    }
 }
