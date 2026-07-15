529784a1636f42fccf027388cad933933ada2ade
Muhammad Hamza Kakar <git200331@gmail.com>
2026-07-15 16:12:53 +0500

    feat: load initial data in home view model

--- feature/home/home-impl/src/main/java/com/example/home_impl/presentation/viewmodel/HomeViewModel.kt
+++ feature/home/home-impl/src/main/java/com/example/home_impl/presentation/viewmodel/HomeViewModel.kt
@@ -1,27 +1,67 @@
 package com.example.home_impl.presentation.viewmodel
 
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.common.base.BaseEvent
 import com.example.common.base.BaseIntent
 import com.example.common.base.BaseState
 import com.example.common.base.BaseViewModel
+import com.example.home_impl.domain.model.Article
+import com.example.home_impl.domain.model.TopHeadlines
 import com.example.home_impl.domain.usecase.GetTopHeadlinesUseCase
+import com.example.network.NetworkResult
+import kotlinx.coroutines.launch
 
-class HomeViewModel (
-    private val getTOpHeadlinesUseCase: GetTopHeadlinesUseCase
-): BaseViewModel<HomeState, HomeIntent, HomeEvent>(
+class HomeViewModel(
+    private val getTopHeadlinesUseCase: GetTopHeadlinesUseCase
+) : BaseViewModel<HomeState, HomeIntent, HomeEvent>(
     HomeState()
 ) {
 
     init {
-        getTOpHeadlinesUseCase
+        loadData()
+    }
+
+    fun loadData() {
+        loadTopHeadlines()
+    }
+
+    fun loadTopHeadlines() = viewModelScope.launch {
+        setState {
+            copy(isLoading = true)
+        }
+        getTopHeadlinesUseCase().collect { result ->
+            when (result) {
+                is NetworkResult.Error -> {
+                    setState {
+                        copy(
+                            isLoading = false,
+                            error = result.message
+                        )
+                    }
+                }
+
+                is NetworkResult.Success<TopHeadlines> -> {
+                    setState {
+                        copy(
+                            isLoading = false,
+                            articles = result.data.articles
+                        )
+                    }
+                }
+            }
+        }
     }
 
     override suspend fun handleIntent(intent: HomeIntent) {
         when (intent) {
             HomeIntent.OnCategoryClick -> {
+
             }
+
             HomeIntent.OnNewsCardClick -> {
             }
+
             HomeIntent.OnSearchClick -> {
             }
         }
@@ -30,18 +70,15 @@ class HomeViewModel (
 
 data class HomeState(
     val isLoading: Boolean = false,
-    val newsImage: String = "",
-    val newsTitle: String =  "",
-    val newsDescription: String = "",
-    val newsAuthor: String = "",
-    val newsPublishedAt: String = "",
-): BaseState
+    val articles: List<Article> = emptyList(),
+    val error: String? = null
+) : BaseState
 
 sealed class HomeIntent : BaseIntent {
-    data object OnNewsCardClick: HomeIntent()
-    data object OnSearchClick: HomeIntent()
-    data object OnCategoryClick: HomeIntent()
+    data object OnNewsCardClick : HomeIntent()
+    data object OnSearchClick : HomeIntent()
+    data object OnCategoryClick : HomeIntent()
 }
 
 sealed class HomeEvent(
-): BaseEvent
+) : BaseEvent
