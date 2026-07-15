7026f43e4219f60cfab8465270b608622abeae7b
Muhammad Hamza Kakar <git200331@gmail.com>
2026-07-15 12:46:05 +0500

    fix: home view model

--- feature/home/home-impl/src/main/java/com/example/home_impl/presentation/viewmodel/HomeViewModel.kt
+++ feature/home/home-impl/src/main/java/com/example/home_impl/presentation/viewmodel/HomeViewModel.kt
@@ -1,10 +1,42 @@
 package com.example.home_impl.presentation.viewmodel
 
+import com.example.common.base.BaseEvent
+import com.example.common.base.BaseIntent
+import com.example.common.base.BaseState
 import com.example.common.base.BaseViewModel
 
-class HomeViewModel: BaseViewModel<LoginState, LoginIntent, LoginEvent>() {
+class HomeViewModel: BaseViewModel<HomeState, HomeIntent, HomeEvent>(
+    HomeState()
+) {
 
-    override suspend fun handleIntent(intent: LoginIntent) {
+    override suspend fun handleIntent(intent: HomeIntent) {
+        when (intent) {
+            HomeIntent.OnCategoryClick -> {
+            }
+            HomeIntent.OnNewsCardClick -> {
 
+            }
+            HomeIntent.OnSearchClick -> {
+
+            }
+        }
     }
 }
+
+data class HomeState(
+    val isLoading: Boolean = false,
+    val newsImage: String = "",
+    val newsTitle: String =  "",
+    val newsDescription: String = "",
+    val newsAuthor: String = "",
+    val newsPublishedAt: String = "",
+): BaseState
+
+sealed class HomeIntent : BaseIntent {
+    data object OnNewsCardClick: HomeIntent()
+    data object OnSearchClick: HomeIntent()
+    data object OnCategoryClick: HomeIntent()
+}
+
+sealed class HomeEvent(
+): BaseEvent
